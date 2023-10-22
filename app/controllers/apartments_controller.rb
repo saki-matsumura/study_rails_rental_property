66ce85790c322450487f3b0a56ab9b2584980f7b
“MatsumuraSaki” <massaki3156@gmail.com>
2023-10-22 20:42:02 +0900

    新規作成機能の実装

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,2 +1,61 @@
 class ApartmentsController < ApplicationController
+  before_action :set_apartment, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @apartments = Apartment.all
+  end
+
+  def new
+    @apartment = Apartment.new
+    @station_index = 0
+    2.times { @apartment.stations.build }
+  end
+
+  def create
+    @apartment = Apartment.new(apartment_params)
+    
+    if @apartment.save
+      redirect_to apartments_path, notice: "物件情報を登録しました！"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    #ここがポイントになる。追加した時にアレをそれするやつ。
+  end
+
+  def update
+  end
+
+  def destroy
+    @apartment.destroy
+    redirect_to apartments_path, notice: "物件情報を削除しました！"
+  end
+
+  private
+
+  def apartment_params
+    params.require(:apartment).permit(:apartment_name,
+                                      :rent,
+                                      :address,
+                                      :building_age,
+                                      :remark)
+  end
+
+  def station_params
+    params.repuire(:station).permit(:line,
+                                    :station_name,
+                                    :minutes_on_foot)
+  end
+
+  def set_apartment
+    @apartment = Apartment.find(params[:id])
+  end
+
+  def set_station
+  end
 end
--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,2 +1,10 @@
 class Apartment < ApplicationRecord
+  has_many :stations
+  accepts_nested_attributes_for :stations
+
+  validates :apartment_name, presence: true
+  validates :rent, presence: true
+  validates :address, presence: true
+  validates :building_age, presence: true
+  validates :remark, presence: true
 end
